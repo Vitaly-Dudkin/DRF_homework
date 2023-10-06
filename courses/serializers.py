066a6f074b8a45079be9df5e3e608e4d8f4e3e2d
vitaly <dudkin231@mail.ru>
2023-10-06 17:22:34 +0300

    added PaymentListAPIView and PaymentSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from courses.models import Course, Lesson
+from courses.models import Course, Lesson, Payment
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -12,6 +12,7 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lesson = LessonSerializer(source='lesson_set', read_only=True, many=True)
+
     def get_lesson_count(self, instance):
         return instance.lesson_set.count()
 
@@ -19,3 +20,8 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ('name', 'preview', 'description', 'lesson', 'lesson_count',)
 
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
 
-from courses.models import Course, Lesson
-from courses.serializers import CourseSerializer, LessonSerializer
+from courses.models import Course, Lesson, Payment
+from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 
 
 # Create your views here.
@@ -31,3 +34,16 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+
+
+class PaymentListAPIView(ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('lesson', 'course', 'payment_method')
+    ordering_fields = ('date_of_payment',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        return queryset.filter(user=self.request.user)
