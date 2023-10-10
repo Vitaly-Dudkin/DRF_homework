65c81355feddddbfdbf24ccee43e348257e8139a
vitaly <dudkin231@mail.ru>
2023-10-10 16:39:31 +0300

    added changes

--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Course, Lesson, Payment
 from courses.permissions import IsModerator, IsOwner
@@ -31,13 +32,13 @@ class CourseViewSet(viewsets.ModelViewSet):
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsModerator]
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwner & ~IsModerator]
+    permission_classes = [IsOwner | ~IsModerator]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
