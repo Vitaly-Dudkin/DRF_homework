89acb1a5c024ac85344765476dd0bd143d9ce688
vitaly <dudkin231@mail.ru>
2023-10-14 16:00:06 +0300

    added subscribe_to_updates in views.py

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 
 from courses.apps import CoursesConfig
 from courses.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView, subscribe_to_updates
 
 router = routers.DefaultRouter()
 router.register(r'courses', CourseViewSet)
@@ -11,10 +11,14 @@ router.register(r'courses', CourseViewSet)
 app_name = CoursesConfig.name
 
 urlpatterns = [
-    path('create/', LessonCreateAPIView.as_view(), name='create'),
-    path('list/', LessonListAPIView.as_view(), name='list'),
-    path('retrieve/<int:pk>/', LessonRetrieveAPIView.as_view(), name='retrieve'),
-    path('update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update'),
-    path('destroy/<int:pk>/', LessonDestroyAPIView.as_view(), name='destroy'),
+    path('lesson_create/', LessonCreateAPIView.as_view(), name='create'),
+    path('lesson_list/', LessonListAPIView.as_view(), name='list'),
+    path('lessonn_detail/<int:pk>/', LessonRetrieveAPIView.as_view(), name='detail'),
+    path('lesson_update/<int:pk>/', LessonUpdateAPIView.as_view(), name='update'),
+    path('lesson_delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='delete'),
+
+    path('payment/<int:pk>/', PaymentListAPIView.as_view(), name='payment'),  # how does it work?
+    path('subscribe/<int:pk>/', subscribe_to_updates, name='subscribe_to_updates')
+
 ] + router.urls
 
--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,16 @@
+from django.http import HttpRequest
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from courses.models import Course, Lesson, Payment
+from courses.models import Course, Lesson, Payment, Subscription
 from courses.paginators import CoursesPaginator
 from courses.permissions import IsModerator, IsOwner
-from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 
 
 # Create your views here.
@@ -77,3 +80,30 @@ class PaymentListAPIView(ListAPIView):
         queryset = super().get_queryset()
 
         return queryset.filter(user=self.request.user)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def subscribe_to_updates(request: HttpRequest, pk: int) -> Response:
+    """
+    Submission to subscribe to course updates or unsubscribe.
+    Prohibited for unauthorized users
+    """
+
+    try:
+        course = Course.objects.get(pk=pk)
+    except Course.DoesNotExist:
+        return Response({"error": "This course doesn't exist."}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        if Subscription.objects.filter(course=course, user=request.user).exists():
+            Subscription.objects.filter(course=course, user=request.user).delete()
+            return Response({'message': f'Subscribe for course updates {course.name} has been cancelled!'},
+                            status=status.HTTP_200_OK)
+
+        data = {'course': pk, 'user': request.user.id}
+        serializer = SubscriptionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': f'Subscribe for course - {course.name}, updates !'},
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
